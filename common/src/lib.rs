045738bb50bb0e238e4f8055afd9d52df5600a8a
Szmoleniczki Ákos <szmoleniczki.akos@edu.bme.hu>
2026-03-08 14:05:16 +0100

    split project into common, languages and cli crates

---
+++ common/src/lib.rs
@@ -0,0 +1,36 @@
+use tree_sitter::{Language, Parser, QueryCursor};
+
+/*
+pub struct SourceFragment {
+    pub text: ,
+    pub start: 
+}
+*/
+
+/*
+pub trait LanguageSupport {
+    fn get_grammar(&self) -> Language;
+    fn get_comment_query_string(&self) -> &str;
+    fn clean_comment(raw: &str) -> String;
+}
+
+pub struct CommentExtractor {
+    parser: Parser,
+    cursor: QueryCursor
+}
+
+impl CommentExtractor {
+    pub fn new() -> Self {
+        Self { 
+            parser: Parser::new(), 
+            cursor: QueryCursor::new() 
+        }
+    }
+
+    /* 
+    pub fn extract(&mut self, language_support: &dyn LanguageSupport, source: &str) -> Vec<Comment> {
+
+    }
+    */
+}
+*/
